35deb69670cc41f945c3d8104d6a8dc27e129bb0
Dawid Wieclaw <dawid.wieclaw@epitech.eu>
2026-04-14 11:45:23 +0200

    Add cypress func tests

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,16 @@
+describe("Index", () => {
+    it("Show index page" ,() => {
+        cy.visit("/")
+        cy.get("body").should("be.visible")
+    })
+    it("Get page content", () => {
+        cy.visit("/")
+        cy.get("body").contains("PLAY")
+        cy.get("body").contains("QUIT")
+    })
+    it("Start the game when PLAY is clicked", () => {
+        cy.visit("/")
+        cy.contains("PLAY").click()
+        cy.contains("PLAY").should("not.be.visible")
+    })
+})
